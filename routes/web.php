bcd00389d53080fa60b7c5427bfc10b812233515
ichiro <ichiro@0xlancerlab.com>
2024-05-07 19:53:20 +0900

    homecontroller fixed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 Route::get( '/', [ AuthController::class, 'show' ] )->name('auth');
 Route::get( '/auth', [ AuthController::class, 'show' ] )->name('auth');
