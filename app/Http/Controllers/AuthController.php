214c2d5ba2ffaafb3c06ad90014c7e34f46b46b9
ichiro <ichiro@0xlancerlab.com>
2024-05-09 00:14:20 +0900

    register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 
 class AuthController extends Controller {
     public function show() {
@@ -17,7 +20,31 @@ class AuthController extends Controller {
         $password = $request->input( 'password' );
 
         if ( Auth::attempt( [ 'company_name' => $company_name, 'team_number' => $team_number, 'password' => $password ] ) ) {
-            return redirect()->intended('home');
+            return redirect()->intended( 'home' );
+        } else {
+            return redirect()->route( 'login' );
+        }
+    }
+
+    public function register ( Request $request ) {
+        $company_name = $request->input( 'company_name' );
+        $team_number = $request->input( 'team_number' );
+        $password = $request->input( 'password' );
+
+        $count = User::where( 'company_name', $company_name )->where( 'team_number', $team_number )->count();
+        if ( $count > 0 ) {
+            return redirect()->route( 'login', [ 'msg' => 'user already exists' ] );
+        } else {
+            $user = new User;
+
+            $user->company_name = $company_name;
+            $user->team_number = $team_number;
+            $user->password = Hash::make( $password );
+            $user->group_id = '';
+
+            $user->save();
+
+            return redirect()->route( 'login' );
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
-Route::get( '/', [ AuthController::class, 'show' ] )->name( 'auth' );
-Route::post( '/login', [ AuthController::class, 'authenticate' ] )->withoutMiddleware( [ VerifyCsrfToken::class ] )->name( 'login' );
+Route::get( '/', [ AuthController::class, 'show' ] )->name( 'login' );
+Route::post( '/authenticate', [ AuthController::class, 'authenticate' ] )->withoutMiddleware( [ VerifyCsrfToken::class ] )->name( 'authenticate' );
+Route::post( '/register', [ AuthController::class, 'register' ] )->withoutMiddleware( [ VerifyCsrfToken::class ] )->name( 'register' );
 
 Route::middleware(['auth'])->group( function() {
     Route::get( '/home', [ HomeController::class, 'show' ] )->name( 'home' );
 });
 Route::post('/uploadPhoto', [ HomeController::class, 'uploadPhoto' ])->withoutMiddleware( [ VerifyCsrfToken::class ] )->name('uploadPhoto');
 Route::get( '/getPhotosByUser', [ HomeController::class, 'getPhotosByUser' ] )->name( 'getPhotosByUser' );
-
