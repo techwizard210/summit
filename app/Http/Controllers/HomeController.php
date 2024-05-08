4e8872a8fa197c4017fde63a73e80fff5e7a575f
ichiro <ichiro@0xlancerlab.com>
2024-05-09 06:44:23 +0900

    disable home router

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
+use ZipArchive;
 
 use App\Models\Photo;
 
@@ -35,8 +36,42 @@ class HomeController extends Controller {
     }
 
     public function getPhotosByUser( Request $request ) {
+        // $user_id = 1;
+        // $paths = Photo::where( 'user_id', $user_id )->pluck( 'path' );
+        // return $paths;
+    }
+
+    public function downloadFolder() {
         $user_id = 1;
-        $paths = Photo::where( 'user_id', $user_id )->pluck( 'path' );
-        return $paths;
+        $folderName = $user_id;
+        $zipFileName = $user_id . '.zip';
+        $zipFilePath = storage_path( 'app/public/' . $zipFileName );
+        $folderPath = storage_path( 'app/public/images/' . $folderName );
+        $zip = new ZipArchive();
+
+        if ( $zip->open( $zipFilePath, ZipArchive::CREATE | ZipArchive::OVERWRITE ) === true ) {
+            // Get all files in the folder
+            $files = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator( $folderPath ),
+                \RecursiveIteratorIterator::LEAVES_ONLY
+            );
+
+            // Add files to the ZIP archive
+            foreach ( $files as $name => $file ) {
+                if ( !$file->isDir() ) {
+                    // Get real and relative paths for current file
+                    $filePath = $file->getRealPath();
+                    $relativePath = substr( $filePath, strlen( $folderPath ) + 1 );
+
+                    // Add file to the ZIP archive
+                    $zip->addFile( $filePath, $relativePath );
+                }
+            }
+
+            // Close the ZIP archive
+            $zip->close();
+        }
+
+        return response()->download( $zipFilePath )->deleteFileAfterSend( true );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ Route::get( '/', [ AuthController::class, 'show' ] )->name( 'login' );
 Route::post( '/authenticate', [ AuthController::class, 'authenticate' ] )->withoutMiddleware( [ VerifyCsrfToken::class ] )->name( 'authenticate' );
 Route::post( '/register', [ AuthController::class, 'register' ] )->withoutMiddleware( [ VerifyCsrfToken::class ] )->name( 'register' );
 
-Route::middleware(['auth'])->group( function() {
+// Route::middleware(['auth'])->group( function() {
     Route::get( '/home', [ HomeController::class, 'show' ] )->name( 'home' );
-});
+// });
 Route::post('/uploadPhoto', [ HomeController::class, 'uploadPhoto' ])->withoutMiddleware( [ VerifyCsrfToken::class ] )->name('uploadPhoto');
 Route::get( '/getPhotosByUser', [ HomeController::class, 'getPhotosByUser' ] )->name( 'getPhotosByUser' );
+Route::get('/downloadFolder', [HomeController::class, 'downloadFolder'])->name('downloadFolder');
