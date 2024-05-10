c4c5a0efd3ec99bccfc00701819209292eaec367
ichiro <ichiro@0xlancerlab.com>
2024-05-11 07:13:17 +0900

    admin gallery

--- resources/views/components/photo-modal.blade.php
+++ resources/views/components/photo-modal.blade.php
@@ -1,5 +1,5 @@
 <div id="modal-{{ $id }}" class="photo-modal">
-    <div class="modal-content" id="content-{{ $id }}">
+    <div class="photo-modal-content" id="content-{{ $id }}">
         <div class="modal-header">
             <span class="header-span">Modal Header</span>
             <span id="close-{{ $id }}" class="close">&times;</span>
@@ -13,13 +13,8 @@
     $(document).ready(function() {
         var modal = document.getElementById("modal-" + <?php echo $id; ?>);
 
-        // Get the <span> element that closes the modal
         var span = document.getElementById("close-" + <?php echo $id; ?>);
 
-        // When the user clicks the button, open the modal 
-
-
-        // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
         }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,15 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Summit</title>
-    <!-- styles -->
-    <link rel="stylesheet" href="{{ asset('/css/components.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
-    <!-- Fonts -->
-
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/svg/logo.svg') }}" />
-    <!-- Custom CSS -->
+
+    
+    <link rel="stylesheet" href="{{ asset('/css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
     <link rel="stylesheet" type="text/css"
         href="{{ asset('/template/assets/libs/select2/dist/css/select2.min.css') }}" />
     <link rel="stylesheet" type="text/css"
@@ -23,19 +21,15 @@
         href="{{ asset('/template/assets/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('/template/assets/libs/quill/dist/quill.snow.css') }}" />
     <link href="{{ asset('/template/dist/css/style.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/template/assets/libs/magnific-popup/dist/magnific-popup.css') }}" rel="stylesheet" />
+
     <script src="{{ asset('/template/assets/libs/jquery/dist/jquery.min.js') }}"></script>
-    <!-- Bootstrap tether Core JavaScript -->
     <script src="{{ asset('/template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
     <script src="{{ asset('/template/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
     <script src="{{ asset('/template/assets/extra-libs/sparkline/sparkline.js') }}"></script>
-    <!--Wave Effects -->
     <script src="{{ asset('/template/dist/js/waves.js') }}"></script>
-    <!--Menu sidebar -->
     <script src="{{ asset('/template/dist/js/sidebarmenu.js') }}"></script>
-    <!--Custom JavaScript -->
     <script src="{{ asset('/template/dist/js/custom.min.js') }}"></script>
-    <!-- This Page JS -->
     <script src="{{ asset('/template/assets/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js') }}"></script>
     <script src="{{ asset('/template/dist/js/pages/mask/mask.init.js') }}"></script>
     <script src="{{ asset('/template/assets/libs/select2/dist/js/select2.full.min.js') }}"></script>
@@ -46,6 +40,8 @@
     <script src="{{ asset('/template/assets/libs/jquery-minicolors/jquery.minicolors.min.js') }}"></script>
     <script src="{{ asset('/template/assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
     <script src="{{ asset('/template/assets/libs/quill/dist/quill.min.js') }}"></script>
+    <script src="{{ asset('/template/assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('/template/assets/libs/magnific-popup/meg.init.js') }}"></script>
     <script src="{{ asset('/js/main.js') }}"></script>
 
 </head>
